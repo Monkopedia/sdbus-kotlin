73d5d0e31bc5ad0357ca578de410375246106d3a
Jason Monk <monkopedia@gmail.com>
2024-07-25 20:26:57 -0400

    Add proper license headers

--- src/commonMain/kotlin/com/monkopedia/sdbus/Error.kt
+++ src/commonMain/kotlin/com/monkopedia/sdbus/Error.kt
@@ -1,3 +1,27 @@
+/**
+ *
+ * (C) 2016 - 2021 KISTLER INSTRUMENTE AG, Winterthur, Switzerland
+ * (C) 2016 - 2024 Stanislav Angelovic <stanislav.angelovic@protonmail.com>
+ * (C) 2024 - 2024 Jason Monk <monkopedia@gmail.com>
+ *
+ * Project: sdbus-kotlin
+ * Description: High-level D-Bus IPC kotlin library based on sd-bus
+ *
+ * This file is part of sdbus-kotlin.
+ *
+ * sdbus-kotlin is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * sdbus-kotlin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with sdbus-kotlin. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.monkopedia.sdbus
 
 class Error(val name: String, val errorMessage: String = "") : Exception("$name: $errorMessage")
