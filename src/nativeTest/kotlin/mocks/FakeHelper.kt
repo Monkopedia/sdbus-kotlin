be1ece242c39daf15b9ff9cb168e6426332067f7
Jason Monk <monkopedia@gmail.com>
2025-02-24 09:45:36 -0500

    Manually fix up license headers

--- src/nativeTest/kotlin/mocks/FakeHelper.kt
+++ src/nativeTest/kotlin/mocks/FakeHelper.kt
@@ -2,7 +2,7 @@
  *
  * (C) 2016 - 2021 KISTLER INSTRUMENTE AG, Winterthur, Switzerland
  * (C) 2016 - 2024 Stanislav Angelovic <stanislav.angelovic@protonmail.com>
- * (C) 2024 - 2024 Jason Monk <monkopedia@gmail.com>
+ * (C) 2024 - 2025 Jason Monk <monkopedia@gmail.com>
  *
  * Project: sdbus-kotlin
  * Description: High-level D-Bus IPC kotlin library based on sd-bus
@@ -19,30 +19,6 @@
  *
  * You should have received a copy of the GNU Lesser General Public License along with
  * sdbus-kotlin. If not, see <https://www.gnu.org/licenses/>.
- */
-/**
- *
- * (C) 2016 - 2021 KISTLER INSTRUMENTE AG, Winterthur, Switzerland
- * (C) 2016 - 2024 Stanislav Angelovic <stanislav.angelovic@protonmail.com>
- * (C) 2024 - 2024 Jason Monk <monkopedia@gmail.com>
- *
- * Project: sdbus-kotlin
- * Description: High-level D-Bus IPC kotlin library based on sd-bus
- *
- * This file is part of sdbus-kotlin.
- *
- * sdbus-kotlin is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * sdbus-kotlin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with sdbus-kotlin. If not, see <http://www.gnu.org/licenses/>.
  */
 @file:Suppress("UNCHECKED_CAST", "IMPLICIT_CAST_TO_ANY")
 
